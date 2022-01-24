c804775028a4f046be866e978e8344a19cd78fb9
Ben Skinner <benjamin.skinner96@gmail.com>
2022-01-24 18:08:57 +1100

    fix: setuptools issues v0.1.1

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ setuptools.setup(name='svg_to_usd',
                  version=Version('0.1.0').number,
                  description="Convert SVG vectors to Pixar's Universal Scene Description",
                  long_description=open('README.md').read().strip(),
+                 packages=setuptools.find_packages(),
                  author='Ben Skinner',
                  author_email='ben.vochsel@gmail.com',
                  url='https://github.com/Vochsel/svg_to_usd',
                  py_modules=['svg_to_usd'],
-                 install_requires=[],
+                 install_requires=[
+                    'usd-core',
+                    'matplotlib',
+                    'fontTools',
+                 ],
                  license='MIT License',
-                 zip_safe=False,
+                 zip_safe=True,
                  keywords='usd, svg',
-                 classifiers=['SVG', 'USD'])
+                 classifiers=['SVG', 'USD']
+)
