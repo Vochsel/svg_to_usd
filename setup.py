b54a40682da7b768cb27583b13c6aa3dd49bf2cf
Ben Skinner <benjamin.skinner96@gmail.com>
2022-02-02 10:18:26 +1100

    fix: pip deps

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from svg_to_usd.version import Version
 
 setuptools.setup(name='svg_to_usd',
-                 version=Version('0.1.3').number,
+                 version=Version('0.1.4').number,
                  description="Convert SVG vectors to Pixar's Universal Scene Description",
                  long_description=open('README.md').read().strip(),
                  packages=setuptools.find_packages(),
