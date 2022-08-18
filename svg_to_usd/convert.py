62eaf42d9eefab50325ad5bae2155d56058f2221
Michael Lyndon <michael.lyndon@yahooinc.com>
2022-08-18 12:36:24 -0400

    remove lines that reset glyphSet to None.

--- svg_to_usd/convert.py
+++ svg_to_usd/convert.py
@@ -1,24 +1,27 @@
 from pxr import Usd
+import importlib
 
 from .converter import common, utils
 from .converter import conversion_context, conversion_options
 
+importlib.reload(common)
 import os
 
 
 def convert_new(svg_path, usd_path):
     stage = Usd.Stage.CreateNew(usd_path)
 
-    conversion_context['working_directory'] = os.path.dirname(usd_path)
-    
+    conversion_context["working_directory"] = os.path.dirname(usd_path)
+
     convert(svg_path, stage)
-    
+
     stage.Save()
     return stage
 
 
 def convert(svg_path, usd_stage, svg_str=None):
     import xml.etree.ElementTree as ET
+
     root = ""
     if svg_str:
         root = ET.fromstring(svg_str)
@@ -29,29 +32,31 @@ def convert(svg_path, usd_stage, svg_str=None):
     i = 0
     for el in root.iter():
         el.set("tree_id", i)
-        i +=1
+        i += 1
 
     if svg_str:
-        common.parent_map = {c: p for p in root.getiterator() for c in p} 
+        common.parent_map = {c: p for p in root.getiterator() for c in p}
     else:
         common.parent_map = {c: p for p in tree.iter() for c in p}
 
     # Setup utils
 
-    conversion_context['texture_directory'] = os.path.join(conversion_context['working_directory'], 'tex')
-    os.makedirs(conversion_context['texture_directory'], exist_ok=True)
+    conversion_context["texture_directory"] = os.path.join(
+        conversion_context["working_directory"], "tex"
+    )
+    os.makedirs(conversion_context["texture_directory"], exist_ok=True)
 
-    if conversion_options['up_axis'] == "x":
+    if conversion_options["up_axis"] == "x":
         utils.convert_position = utils.convert_position_x
-    elif conversion_options['up_axis'] == "y":
+    elif conversion_options["up_axis"] == "y":
         utils.convert_position = utils.convert_position_y
-    elif conversion_options['up_axis'] == "z":
+    elif conversion_options["up_axis"] == "z":
         utils.convert_position = utils.convert_position_z
 
-    if 'width' in root.attrib:
-        conversion_context['document_width'] = root.attrib['width']
-    if 'height' in root.attrib:
-        conversion_context['document_height'] = root.attrib['height']
+    if "width" in root.attrib:
+        conversion_context["document_width"] = root.attrib["width"]
+    if "height" in root.attrib:
+        conversion_context["document_height"] = root.attrib["height"]
 
     common.preprocess_svg_root(usd_stage, root)
     common.handle_svg_root(usd_stage, root)
--- svg_to_usd/converter/geometry/text.py
+++ svg_to_usd/converter/geometry/text.py
@@ -12,15 +12,14 @@ from svgpath2mpl import parse_path
 
 
 class SVGPen(BasePen):
-
     def __init__(self, glyphSet):
         BasePen.__init__(self, glyphSet)
-        self.d = u''
+        self.d = ""
         self._lastX = self._lastY = None
 
     def _moveTo(self, pt):
         ptx, pty = self._isInt(pt)
-        self.d += u'M{} {}'.format(ptx, pty)
+        self.d += "M{} {}".format(ptx, pty)
         self._lastX, self._lastY = pt
 
     def _lineTo(self, pt):
@@ -28,29 +27,28 @@ class SVGPen(BasePen):
         if (ptx, pty) == (self._lastX, self._lastY):
             return
         elif ptx == self._lastX:
-            self.d += u'V{}'.format(pty)
+            self.d += "V{}".format(pty)
         elif pty == self._lastY:
-            self.d += u'H{}'.format(ptx)
+            self.d += "H{}".format(ptx)
         else:
-            self.d += u'L{} {}'.format(ptx, pty)
+            self.d += "L{} {}".format(ptx, pty)
         self._lastX, self._lastY = pt
 
     def _curveToOne(self, pt1, pt2, pt3):
         pt1x, pt1y = self._isInt(pt1)
         pt2x, pt2y = self._isInt(pt2)
         pt3x, pt3y = self._isInt(pt3)
-        self.d += u'C{} {} {} {} {} {}'.format(pt1x, pt1y, pt2x, pt2y,
-                                               pt3x, pt3y)
+        self.d += "C{} {} {} {} {} {}".format(pt1x, pt1y, pt2x, pt2y, pt3x, pt3y)
         self._lastX, self._lastY = pt3
 
     def _qCurveToOne(self, pt1, pt2):
         pt1x, pt1y = self._isInt(pt1)
         pt2x, pt2y = self._isInt(pt2)
-        self.d += u'Q{} {} {} {}'.format(pt1x, pt1y, pt2x, pt2y)
+        self.d += "Q{} {} {} {}".format(pt1x, pt1y, pt2x, pt2y)
         self._lastX, self._lastY = pt2
 
     def _closePath(self):
-        self.d += u'Z'
+        self.d += "Z"
         self._lastX = self._lastY = None
 
     def _endPath(self):
@@ -77,26 +75,26 @@ def get_font_properties(element):
         Font properties suitable for use in creating a Font instance.
     """
 
-    svg_font_family = "Arial" # TODO: Configurable fallback font
-    svg_font_weight = None 
-    svg_font_style = 0 
+    svg_font_family = "Arial"  # TODO: Configurable fallback font
+    svg_font_weight = None
+    svg_font_style = 0
     svg_font_size = 144
 
-    if 'font-family' in element.attrib:
-        svg_font_family = element.attrib['font-family']
+    if "font-family" in element.attrib:
+        svg_font_family = element.attrib["font-family"]
 
-    if 'font-weight' in element.attrib:
-        svg_font_weight = element.attrib['font-weight'].lower()
+    if "font-weight" in element.attrib:
+        svg_font_weight = element.attrib["font-weight"].lower()
         try:
             svg_font_weight = font.WEIGHTS[svg_font_weight]
         except:
-            svg_font_weight = int(svg_font_weight) 
+            svg_font_weight = int(svg_font_weight)
 
-    if 'font-size' in element.attrib:
-        svg_font_size = int(element.attrib['font-size'])
+    if "font-size" in element.attrib:
+        svg_font_size = int(element.attrib["font-size"])
 
-    if 'font-style' in element.attrib:
-        if element.attrib['font-style'] == 'italic':
+    if "font-style" in element.attrib:
+        if element.attrib["font-style"] == "italic":
             svg_font_style = 2
 
     if svg_font_weight == None:
@@ -106,8 +104,9 @@ def get_font_properties(element):
         "family": svg_font_family,
         "size": svg_font_size,
         "weight": svg_font_weight,
-        "style": svg_font_style
+        "style": svg_font_style,
     }
+
 
 def create_usd_text_mesh(word, glyphSet, cmap, usd_mesh, units_per_em, font_size):
     svg_d = ""
@@ -121,10 +120,10 @@ def create_usd_text_mesh(word, glyphSet, cmap, usd_mesh, units_per_em, font_size
         try:
             glyph = glyphSet[cmap[ord(c)]]
         except:
-            glyph = glyphSet['.notdef']
+            glyph = glyphSet[".notdef"]
             continue
 
-        if c == ' ':
+        if c == " ":
             _charXOffset += glyph.width
             continue
 
@@ -145,7 +144,14 @@ def create_usd_text_mesh(word, glyphSet, cmap, usd_mesh, units_per_em, font_size
         svg_path = parse_path(svg_d)
 
         usd_points, usd_fvi, usd_fvc = utils.path_to_mesh(
-            svg_path, usd_points, usd_fvi, usd_fvc, _charXOffset, 0, 1.0/(units_per_em)*font_size)
+            svg_path,
+            usd_points,
+            usd_fvi,
+            usd_fvc,
+            _charXOffset,
+            0,
+            1.0 / (units_per_em) * font_size,
+        )
 
         _charXOffset += glyph.width
 
@@ -162,16 +168,24 @@ def convert(usd_stage, prim_path, svg_text, fallback_font, type):
     elif type == "schema":
         return convert_as_schema(usd_stage, prim_path, svg_text, fallback_font)
 
+
 def convert_as_schema(usd_stage, prim_path, svg_text, fallback_font):
     logging.debug("Creating text: schema")
-    
+
     font_props = get_font_properties(svg_text)
 
-    #initialise the generalised Font Class instance
-    svg_font = font.Font(face_name=font_props["family"], size=font_props["size"], weight=font_props["weight"], style=font_props["style"])
-    
+    # initialise the generalised Font Class instance
+    svg_font = font.Font(
+        face_name=font_props["family"],
+        size=font_props["size"],
+        weight=font_props["weight"],
+        style=font_props["style"],
+    )
+
     # TODO: The creation of usd_font feels like it could be moved into the font class
-    temp_weight = " {}".format([k for k, v in font.WEIGHTS.items() if svg_font.weight == v][0].title())
+    temp_weight = " {}".format(
+        [k for k, v in font.WEIGHTS.items() if svg_font.weight == v][0].title()
+    )
     if temp_weight == " Regular":
         temp_weight = ""
     temp_style = ""
@@ -188,40 +202,54 @@ def convert_as_schema(usd_stage, prim_path, svg_text, fallback_font):
         if not svg_word:
             continue
 
-        prim = usd_stage.DefinePrim(text_group.GetPath().AppendChild(Tf.MakeValidIdentifier("tspan_{}".format(svg_word))), "Preliminary_Text")
+        prim = usd_stage.DefinePrim(
+            text_group.GetPath().AppendChild(
+                Tf.MakeValidIdentifier("tspan_{}".format(svg_word))
+            ),
+            "Preliminary_Text",
+        )
         prim.CreateAttribute("content", Sdf.ValueTypeNames.String).Set(svg_word)
         prim.CreateAttribute("font", Sdf.ValueTypeNames.StringArray).Set([usd_font])
         prim.CreateAttribute("depth", Sdf.ValueTypeNames.Float).Set(0.0)
-        prim.CreateAttribute("horizontalAlignment", Sdf.ValueTypeNames.String).Set("left")
-        prim.CreateAttribute("verticalAlignment", Sdf.ValueTypeNames.String).Set("middle")
+        prim.CreateAttribute("horizontalAlignment", Sdf.ValueTypeNames.String).Set(
+            "left"
+        )
+        prim.CreateAttribute("verticalAlignment", Sdf.ValueTypeNames.String).Set(
+            "middle"
+        )
         prim.CreateAttribute("pointSize", Sdf.ValueTypeNames.Float).Set(svg_font.size)
         prim.CreateAttribute("wrapMode", Sdf.ValueTypeNames.String).Set("flowing")
         prim.CreateAttribute("width", Sdf.ValueTypeNames.Float).Set(10)
         prim.CreateAttribute("height", Sdf.ValueTypeNames.Float).Set(10)
-        if 'id' in svg_text.attrib:
-            prim.CreateAttribute("id", Sdf.ValueTypeNames.String).Set(svg_text.attrib['id'])
-
+        if "id" in svg_text.attrib:
+            prim.CreateAttribute("id", Sdf.ValueTypeNames.String).Set(
+                svg_text.attrib["id"]
+            )
 
         try:
-            svg_x = float(tspan.attrib['x'])
+            svg_x = float(tspan.attrib["x"])
         except:
             svg_x = 0.0
         try:
-            svg_y = float(tspan.attrib['y'])
+            svg_y = float(tspan.attrib["y"])
         except:
             svg_y = 0.0
 
         xform = UsdGeom.Xform(prim)
-        
+
         xform.AddTransformOp(opSuffix="xy").Set(
-            Gf.Matrix4d(1.0).SetTranslate(Gf.Vec3d(svg_x, 0, svg_y)))
+            Gf.Matrix4d(1.0).SetTranslate(Gf.Vec3d(svg_x, 0, svg_y))
+        )
 
     return text_group
+
 
 def convert_as_geo(usd_stage, prim_path, svg_text, fallback_font):
 
     # Might be a better place to put this. Needed for setting extents later.
-    bboxCache = UsdGeom.BBoxCache(Usd.TimeCode.Default(), includedPurposes=[UsdGeom.Tokens.default_])
+    bboxCache = UsdGeom.BBoxCache(
+        Usd.TimeCode.Default(), includedPurposes=[UsdGeom.Tokens.default_]
+    )
 
     logging.debug("Creating text")
     text_root = None
@@ -233,15 +261,22 @@ def convert_as_geo(usd_stage, prim_path, svg_text, fallback_font):
     t = None
     units_per_em = 2048
 
-    if 'fill' in svg_text.attrib:
-        svg_fill = svg_text.attrib['fill']
+    if "fill" in svg_text.attrib:
+        svg_fill = svg_text.attrib["fill"]
 
-    #Get the base font properties
+    # Get the base font properties
     font_props = get_font_properties(svg_text)
-    #Create a generalised Font instance
-    svg_font = font.Font(face_name=font_props["family"], size=font_props["size"], weight=font_props["weight"], style=font_props["style"])
-    
-    temp_weights = " {}".format([" "+k for k, v in font.WEIGHTS.items() if svg_font.weight == v])
+    # Create a generalised Font instance
+    svg_font = font.Font(
+        face_name=font_props["family"],
+        size=font_props["size"],
+        weight=font_props["weight"],
+        style=font_props["style"],
+    )
+
+    temp_weights = " {}".format(
+        [" " + k for k, v in font.WEIGHTS.items() if svg_font.weight == v]
+    )
     if svg_font.weight == 400:
         temp_weights = [""]
     temp_style = ""
@@ -249,34 +284,35 @@ def convert_as_geo(usd_stage, prim_path, svg_text, fallback_font):
         temp_style = " italic"
     ft_styles = []
     for weight in temp_weights:
-        style = "{}{}".format(weight, temp_style) 
+        style = "{}{}".format(weight, temp_style)
         if style.strip() == "":
             style = "regular"
         style = style.strip()
         ft_styles.append(style)
 
+    # TODO Don't forget to add logic to handle url paths
     if "/" in font_props["family"] or "\\" in font_props["family"]:
         print("font_path", font_props["family"])
         font_path = font_props["family"]
     else:
-        print("Font:", svg_font)
+        # print("Font:", svg_font)
         font_path = svg_font.findfont()
 
     try:
         ftfont = None
         if font_path.endswith("ttc"):
-                fonts = ttLib.TTCollection(font_path)
-                for fnt in fonts:
-                    name = fnt['name']
-                    family = name.getBestFamilyName()
-                    style = name.getBestSubFamilyName()
-                    if family == svg_font.name and style.lower() in ft_styles:
-                        ftfont = fnt 
+            fonts = ttLib.TTCollection(font_path)
+            for fnt in fonts:
+                name = fnt["name"]
+                family = name.getBestFamilyName()
+                style = name.getBestSubFamilyName()
+                if family == svg_font.name and style.lower() in ft_styles:
+                    ftfont = fnt
         else:
             ftfont = ttLib.TTFont(font_path)
-        cmap = ftfont['cmap']
+        cmap = ftfont["cmap"]
         t = cmap.getBestCmap()
-        units_per_em = ftfont['head'].unitsPerEm
+        units_per_em = ftfont["head"].unitsPerEm
 
         gSet = ftfont.getGlyphSet()
         ftfont.close()
@@ -284,21 +320,15 @@ def convert_as_geo(usd_stage, prim_path, svg_text, fallback_font):
     except ttLib.TTLibError:
         logging.error(f"ERROR: {fallback_font} cannot be processed.")
         return 1
-    
-    #Check if the text element has any children. Most likely <tspan> elements.
-    if(len(list(svg_text)) > 1):
-        #Create an xform to hold the tspan elements.
+
+    # Check if the text element has any children. Most likely <tspan> elements.
+    if len(list(svg_text)) > 1:
+        # Create an xform to hold the tspan elements.
         text_root = UsdGeom.Xform.Define(usd_stage, prim_path)
 
-        svg_fill = None
-        font_path = ""
-        gSet = None
-        cmap = None
-        t = None
-
         align = 0
-        if 'text-anchor' in svg_text.attrib:
-            svg_text_anchor = svg_text.attrib['text-anchor']
+        if "text-anchor" in svg_text.attrib:
+            svg_text_anchor = svg_text.attrib["text-anchor"]
             if svg_text_anchor == "middle":
                 align = -2
             elif svg_text_anchor == "end":
@@ -311,21 +341,31 @@ def convert_as_geo(usd_stage, prim_path, svg_text, fallback_font):
             if not svg_word:
                 continue
 
-            usd_mesh = UsdGeom.Mesh.Define(usd_stage, text_root.GetPath().AppendChild(Tf.MakeValidIdentifier("tspan_{}_{}".format(svg_word, tspan.attrib['tree_id']))))
+            usd_mesh = UsdGeom.Mesh.Define(
+                usd_stage,
+                text_root.GetPath().AppendChild(
+                    Tf.MakeValidIdentifier(
+                        "tspan_{}_{}".format(svg_word, tspan.attrib["tree_id"])
+                    )
+                ),
+            )
             # add parent fill colour if none exists
-            if 'fill' not in tspan.attrib:
-                tspan.attrib['fill'] = svg_fill
+            if "fill" not in tspan.attrib:
+                tspan.attrib["fill"] = svg_fill
 
-            utils.handle_geom_attrs(tspan, usd_mesh) 
+            utils.handle_geom_attrs(tspan, usd_mesh)
 
             usd_mesh.AddTransformOp(opSuffix="align").Set(
-                Gf.Matrix4d(1.0).SetTranslate(Gf.Vec3d(align, 0, 0)))
+                Gf.Matrix4d(1.0).SetTranslate(Gf.Vec3d(align, 0, 0))
+            )
 
-            create_usd_text_mesh(svg_word, gSet, t, usd_mesh, units_per_em, svg_font.size)
+            create_usd_text_mesh(
+                svg_word, gSet, t, usd_mesh, units_per_em, svg_font.size
+            )
 
             utils.set_extent(usd_mesh.GetPrim(), bboxCache)
-    
-    #Do this if the text element doesn't have any children elements.
+
+    # Do this if the text element doesn't have any children elements.
     else:
         text_root = UsdGeom.Mesh.Define(usd_stage, prim_path)
 
@@ -336,26 +376,26 @@ def convert_as_geo(usd_stage, prim_path, svg_text, fallback_font):
         if not svg_word:
             # Hack to get first span
             svg_word = svg_text[0].text
-        
+
         if not svg_word:
             return
 
         font_path = fallback_font
 
         align = 0
-        if 'text-anchor' in svg_text.attrib:
-            svg_text_anchor = svg_text.attrib['text-anchor']
+        if "text-anchor" in svg_text.attrib:
+            svg_text_anchor = svg_text.attrib["text-anchor"]
             if svg_text_anchor == "middle":
                 align = -2
             elif svg_text_anchor == "end":
                 align = -4
 
         text_root.AddTransformOp(opSuffix="align").Set(
-            Gf.Matrix4d(1.0).SetTranslate(Gf.Vec3d(align, 0, 0)))
+            Gf.Matrix4d(1.0).SetTranslate(Gf.Vec3d(align, 0, 0))
+        )
 
         create_usd_text_mesh(svg_word, gSet, t, text_root, units_per_em, svg_font.size)
 
         utils.set_extent(text_root.GetPrim(), bboxCache)
 
     return text_root
-
