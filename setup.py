8cb4157b06ed03c3e8d1627c89cfc51b10b12810
Michael Lyndon <michael.lyndon@yahooinc.com>
2022-08-18 15:36:19 -0400

    bump version. merge after PR#6

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 import setuptools
 from svg_to_usd.version import Version
 
-setuptools.setup(name='svg_to_usd',
-                 version=Version('0.1.17').number,
-                 description="Convert SVG vectors to Pixar's Universal Scene Description",
-                 long_description=open('README.md').read().strip(),
-                 packages=setuptools.find_packages(),
-                 author='Ben Skinner',
-                 author_email='ben.vochsel@gmail.com',
-                 url='https://github.com/Vochsel/svg_to_usd',
-                 py_modules=['svg_to_usd'],
-                 install_requires=[
-                    'svgpath2mpl',
-                    'matplotlib',
-                  #   'usd-core',
-                    'opentypesvg',
-                 ],
-                 license='MIT License',
-                 zip_safe=True,
-                 keywords='usd, svg',
-                 classifiers=['SVG', 'USD']
+setuptools.setup(
+    name="svg_to_usd",
+    version=Version("0.1.19").number,
+    description="Convert SVG vectors to Pixar's Universal Scene Description",
+    long_description=open("README.md").read().strip(),
+    packages=setuptools.find_packages(),
+    author="Ben Skinner",
+    author_email="ben.vochsel@gmail.com",
+    url="https://github.com/Vochsel/svg_to_usd",
+    py_modules=["svg_to_usd"],
+    install_requires=[
+        "svgpath2mpl",
+        "matplotlib",
+        #   'usd-core',
+        "opentypesvg",
+    ],
+    license="MIT License",
+    zip_safe=True,
+    keywords="usd, svg",
+    classifiers=["SVG", "USD"],
 )
